5c895146249b673796025912cde46e30e619f404
Tongtong <tqi3@outlook.com>
2023-05-11 22:02:28 -0400

    Initial project add.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class PetType {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
