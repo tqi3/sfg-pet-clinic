75d9cb6160440b7feab38839fce9c2f79849cd38
Tongtong <tqi3@outlook.com>
2023-05-11 23:30:13 -0400

    Closes #1.

--- pet-clinic-data/src/main/java/guru/springframework/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/model/PetType.java
@@ -1,4 +1,4 @@
-package guru.springframework.sfgpetclinic.model;
+package guru.springframework.model;
 
 public class PetType {
     public String getName() {
---
+++ pet-clinic-data/src/main/java/guru/springframework/model/Vet.java
@@ -0,0 +1,5 @@
+package guru.springframework.model;
+
+public class Vet extends Person {
+
+}
